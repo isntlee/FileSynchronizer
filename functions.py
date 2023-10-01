7fa71d0616df5e04b36a187f8cd228c9e240e7c4
DESKTOP-5FRD4TM\Lee <leedavidmartina@gmail.com>
2023-10-01 14:54:42 +0200

    Added sync_mode function

--- functions.py
+++ functions.py
@@ -1,17 +1,31 @@
 import os, shutil, logging, hashlib, logging
 
 
-def pyrobocopy(source, dest):
+def log_info(message):
+    logging.info(message)
+
+
+def sync_mode(source, dest, mode='pyrobocopy'):
     for file in os.listdir(source):
         s = os.path.join(source, file)
         d = os.path.join(dest, file)
         
         if os.path.isfile(s):
-            if not os.path.exists(d) or os.path.getmtime(s) > os.path.getmtime(d):
+            if not os.path.exists(d):
                 shutil.copy2(s, d)
-                log_info(f"Copied file {s} to {d}")
+                print(f"Copied file {s} to {d}")
+                
+            elif mode == 'pyrobocopy':
+                if os.path.getmtime(s) > os.path.getmtime(d):
+                    shutil.copy2(s, d)
+                    print(f"Copied file {s} to {d}")
+
+            elif mode == 'md5':
+                if md5_hashing(s) != md5_hashing(d):
+                    shutil.copy2(s, d)
+                    print(f"Copied file {s} to {d}")
         elif os.path.isdir(s):
-            sync_dirs(s, d)
+            sync_mode(s, d, mode)
 
     for file in os.listdir(dest):
         d = os.path.join(dest, file)
@@ -20,14 +34,18 @@ def pyrobocopy(source, dest):
         if not os.path.exists(s):
             if os.path.isfile(d):
                 os.remove(d)
-                log_info(f"Removed file {d}")
+                print(f"Removed file {d}")
             elif os.path.isdir(d):
                 shutil.rmtree(d)
-                log_info(f"Removed directory {d}")
+                print(f"Removed directory {d}")
 
 
-def log_info(message):
-    logging.info(message)
+def md5_hashing(file_name):
+    hash_md5 = hashlib.md5()
+    with open(file_name, "rb") as f:
+        for chunk in iter(lambda: f.read(4096), b""):
+            hash_md5.update(chunk)
+    return hash_md5.hexdigest()
 
 
 def logger_func(log_file):
@@ -52,21 +70,3 @@ def logger_func(log_file):
     logger.addHandler(console_handler)
 
 
-def md5(fname):
-    hash_md5 = hashlib.md5()
-    with open(fname, "rb") as f:
-        for chunk in iter(lambda: f.read(4096), b""):
-            hash_md5.update(chunk)
-    return hash_md5.hexdigest()
-
-
-def md5(source, dest):
-    file1 = "path/to/file1"
-    file2 = "path/to/file2"
-
-    if md5(file1) == md5(file2):
-        print("The files are identical")
-    else:
-        print("The files are different")
-
-# Add this below -> 
--- syncronize.py
+++ syncronize.py
@@ -1,11 +1,11 @@
-from functions import pyrobocopy, logger_func
+from functions import sync_mode, logger_func
 import argparse
 import time
 
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('method', choices=['pyrobocopy', 'md5'], default='pyrobocopy')
+    parser.add_argument('mode', choices=['pyrobocopy', 'md5'], default='pyrobocopy')
     parser.add_argument('source_dir')
     parser.add_argument('dest_dir')
     parser.add_argument('log_file')
@@ -14,7 +14,7 @@ def main():
     logger_func(args.log_file)
 
     while True:
-        pyrobocopy(args.source_dir, args.dest_dir)
+        sync_mode(args.source_dir, args.dest_dir)
         time.sleep(args.period)
 
 if __name__ == "__main__":
